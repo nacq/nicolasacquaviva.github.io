bc8e0ff5d0844e1f34263ba74dcbf0601c27fbd1
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2020-06-22 00:06:36 -0300

    feat: add socket server and save commands to mongo

---
+++ api/src/models/commands.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+    "context"
+    "log"
+    "time"
+
+    "go.mongodb.org/mongo-driver/mongo"
+)
+
+type Command struct {
+    Name string `json:"name"`
+    ExecutionStatus bool `json:"executionStatus"`
+    Date string `json:"date"`
+}
+
+func (db *DB) SaveCommand(command string, status bool) (*mongo.InsertOneResult, error) {
+    collection := db.Database("personal-site").Collection("commands")
+
+    commandToSave := Command{command, status, time.Now().String()}
+
+    savedCommand, err := collection.InsertOne(context.TODO(), commandToSave)
+
+    if err != nil {
+        log.Println("Error saving command: ", err)
+
+        return nil, err
+    }
+
+    return savedCommand, nil
+}
