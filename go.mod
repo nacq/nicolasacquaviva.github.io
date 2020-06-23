3df802fd72daa8b49a6e557fb06658849a6ec46a
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2020-06-22 21:45:58 -0300

    feat: add socket server to store commands

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nicolasacquaviva/nicolasacquaviva.github.io
+
+go 1.13
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	go.mongodb.org/mongo-driver v1.3.4
+)
---
+++ models/db.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+    "context"
+    "log"
+    "go.mongodb.org/mongo-driver/mongo"
+    // "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Datastore interface {
+    SaveCommand(string, bool, string) (*mongo.InsertOneResult, error)
+}
+
+type DB struct {
+    *mongo.Client
+}
+
+// Uppercased function mame means exportable
+func NewDB(uri string) (*DB, error) {
+    clientOptions := options.Client().ApplyURI(uri)
+    client, err := mongo.Connect(context.TODO(), clientOptions)
+
+    if err != nil {
+        log.Println("Error connecting to DB: ", err)
+
+        return nil, err
+    }
+
+    err = client.Ping(context.TODO(), nil)
+
+    if err != nil {
+        log.Fatal("Cannot reach DB: ", err)
+
+        return nil, err
+    }
+
+    return &DB{client}, nil
+}
