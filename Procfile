44b9ca30d8cb4d198569f8eb83c3d68df8718d39
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2020-06-22 22:30:11 -0300

    Add socket server (#1)
    * feat: add socket server and save commands to mongo
    
    * feat: connect socket and send commands
    
    * fix: send command instead of error
    
    * ref: remove api dir
    
    * chore: add Procfile
    
    * feat: add socket server to store commands
    
    * fix: update Procfile
    
    * ref: read port from env
    
    * Revert "fix: update Procfile"
    
    This reverts commit e42195551bbb002f54cc28fd7507304443eeee13.
    
    * fix: change command in Procfile
    
    * fix: listen to all network connections
    
    * ref: add origin check
    
    * feat: connect js socket to server

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/nicolasacquaviva.github.io
---
+++ models/commands.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+    "context"
+    "log"
+    "time"
+
+    "go.mongodb.org/mongo-driver/mongo"
+)
+
+type Command struct {
+    Name string `json:"name"`
+    ExecutionStatus bool `json:"executionStatus"`
+    Date string `json:"date"`
+    ClientIP string `json:"clientIP"`
+}
+
+func (db *DB) SaveCommand(command string, status bool, clientIP string) (*mongo.InsertOneResult, error) {
+    collection := db.Database("personal-site").Collection("commands")
+
+    commandToSave := Command{command, status, time.Now().String(), clientIP}
+
+    savedCommand, err := collection.InsertOne(context.TODO(), commandToSave)
+
+    if err != nil {
+        log.Println("Error saving command: ", err)
+
+        return nil, err
+    }
+
+    return savedCommand, nil
+}
